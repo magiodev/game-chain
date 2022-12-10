efe870470337e843492a2888f95b696ab30e1c71
0xMagio <31893902+magiodev@users.noreply.github.com>
2022-12-10 19:36:45 +0100

    Implemented x/nft interface methods to expected_keepers.go

--- x/assetfactory/types/expected_keepers.go
+++ x/assetfactory/types/expected_keepers.go
@@ -3,6 +3,7 @@ package types
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/auth/types"
+	"github.com/cosmos/cosmos-sdk/x/nft"
 )
 
 type PermissionKeeper interface {
@@ -14,7 +15,28 @@ type GameKeeper interface {
 }
 
 type NftKeeper interface {
+	// Class
+	SaveClass(ctx sdk.Context, class nft.Class) error
+	UpdateClass(ctx sdk.Context, class nft.Class) error
+
+	// NFT
+	Mint(ctx sdk.Context, nft nft.NFT, receiver sdk.AccAddress) error // updates totalSupply
+	Burn(ctx sdk.Context, classId string, nftId string) error         // updates totalSupply
+	Update(ctx sdk.Context, nft nft.NFT) error
+	Transfer(ctx sdk.Context, classId string, nftId string, receiver sdk.AccAddress) error
+
+	// Getters
+	GetClass(ctx sdk.Context, classId string) (nft.Class, bool)
+	GetClasses(ctx sdk.Context) (classes []*nft.Class)
+	GetNFT(ctx sdk.Context, classID string, nftID string) (nft.NFT, bool)
+	GetNFTsOfClassByOwner(ctx sdk.Context, classId string, owner sdk.AccAddress) []nft.NFT
+	GetNFTsOfClass(ctx sdk.Context, classId string) []nft.NFT
+	GetOwner(ctx sdk.Context, classId string, nftId string) sdk.AccAddress
+	GetBalance(ctx sdk.Context, classId string, owner sdk.AccAddress) uint64
+	GetTotalSupply(ctx sdk.Context, classId string) uint64
 	// Methods imported from nft should be defined here
+
+	nft.QueryServer
 }
 
 // AccountKeeper defines the expected account keeper used for simulations (noalias)
